c68fec07416e1f490a6ae7f5f2cb8df127791d89
Abhishek <kalraabhishek98@gmail.com>
2018-10-17 04:06:20 +0530

    Added basic timer Added basic matrix transformations

--- fx.cpp
+++ fx.cpp
@@ -4,11 +4,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <sstream>
 #include <exception>
 #include <glload/gl_3_3.h>
 #include <glutil/Shader.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace FX {
 
@@ -39,6 +42,76 @@ namespace FX {
             std::cerr << e.what() << std::endl;
             throw;
         }
+    }
+
+    void Timer::Update() {
+        float fTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+        
+        if(m_isPaused) {
+            m_currTime = fTime;
+            return;
+        }
+
+        m_accuTime += m_currTime - fTime;
+        m_currTime = fTime;
+
+        return;
+    }
+
+    float Timer::getAlpha() {
+        return fmodf(m_accuTime, m_secDuration) / m_secDuration;
+    }
+
+    glm::mat4 RotateX(float fAngDeg) {
+        float fAngRad = DegToRad(fAngDeg);
+        float fCos = cosf(fAngRad);
+        float fSin = sinf(fAngRad);
+
+        glm::mat3 theMat(1.0f);
+        theMat[1].y = fCos; theMat[2].y = -fSin;
+        theMat[1].z = fSin; theMat[2].z = fCos;
+
+        return glm::mat4(theMat);
+    }
+
+    glm::mat4 RotateY(float fAngDeg) {
+        float fAngRad = DegToRad(fAngDeg);
+        float fCos = cosf(fAngRad);
+        float fSin = sinf(fAngRad);
+
+        glm::mat3 theMat(1.0f);
+        theMat[0].x = fCos; theMat[2].x = fSin;
+        theMat[0].z = -fSin; theMat[2].z = fCos;
+
+        return glm::mat4(theMat);
+    }
+
+    glm::mat4 RotateZ(float fAngDeg) {
+        float fAngRad = DegToRad(fAngDeg);
+        float fCos = cosf(fAngRad);
+        float fSin = sinf(fAngRad);
+
+        glm::mat3 theMat(1.0f);
+        theMat[0].x = fCos; theMat[1].x = -fSin;
+        theMat[0].y = fSin; theMat[1].y = fCos;
+
+        return glm::mat4(theMat);
+    }
+
+    glm::mat4 Translate(const glm::vec3 &translateVec) {
+        glm::mat4 translateMat(1.0f);
+        translateMat[3] = glm::vec4(translateVec, 1.0f);
+
+        return translateMat;
+    }
+
+    glm::mat4 Scale(const glm::vec3 &scaleVec) {
+        glm::mat4 scaleMat(1.0f);
+        scaleMat[0].x = scaleVec.x;
+        scaleMat[1].y = scaleVec.y;
+        scaleMat[2].z = scaleVec.z;
+
+        return scaleMat;
     }
 
 }
--- fx.h
+++ fx.h
@@ -1,10 +1,60 @@
 #include <string>
 #include <vector>
 #include <glload/gl_3_3.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace FX {
-
+    
     GLuint LoadShaderFromFile(GLenum eShaderType, const std::string& path);
     GLuint CreateProgram(const std::vector<GLuint>& shaderList);
+    
+    
+    class Timer {
+    public:
+        Timer(float loopDuration): m_currTime(glutGet(GLUT_ELAPSED_TIME) / 1000.0f),
+                m_accuTime(0.0f), m_secDuration(loopDuration), m_isPaused(false) {};
+
+        void Update();
+
+        float getAlpha();
+
+    private:
+        float m_currTime;
+        float m_accuTime;
+        float m_secDuration;
+
+        bool m_isPaused;
+    };
+
+
+    inline float DegToRad(float fAngDeg) {
+        const static float fDegToRad = 3.14159f * 2.0f / 360.0f;
+        return fAngDeg * fDegToRad;
+    }
+
+    inline float Clamp(float fValue, float fMinValue, float fMaxValue) {
+        if(fValue < fMinValue)
+            return fMinValue;
+
+        if(fValue > fMaxValue)
+            return fMaxValue;
+
+        return fValue;
+    }
+
+    glm::mat4 RotateX(float fAngDeg);
+    glm::mat4 RotateY(float fAngDeg);
+    glm::mat4 RotateZ(float fAngDeg);
+
+    glm::mat4 Translate(const glm::vec3 &translateVec);
+    
+    glm::mat4 Scale(const glm::vec3 &scaleVec);
+
+    inline float CalcFrustumScale(float FOVdeg) {
+        float FOVrad = DegToRad(FOVdeg);
+        return 1.0f / tan(FOVrad / 2.0f);
+    }
 
 }
